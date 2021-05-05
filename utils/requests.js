75d14696b9a9d6989f8fa158cce2e689d417d144
jay75chauhan <chauhan75jayk@gmail.com>
2021-05-05 17:46:36 +0530

    adding movies

--- utils/requests.js
+++ utils/requests.js
@@ -5,6 +5,10 @@ export default {
     title: "Trending",
     url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   },
+  fetchNetflixOriginals: {
+    title: "Netflix Movie",
+    url: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  },
   fetchTopRated: {
     title: "Top Rated",
     url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
