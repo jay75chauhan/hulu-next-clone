400d8daea74f25d05dc68d2249706f25d284a310
jay75chauhan <chauhan75jayk@gmail.com>
2021-10-10 15:27:50 +0530

    pwa sapport

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
   images: {
     domains: ["image.tmdb.org", "upload.wikimedia.org"],
     distDir: "out",
   },
-};
+});
